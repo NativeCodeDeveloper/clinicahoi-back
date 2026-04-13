b5a2fa678a58d09d1dc7af128727e542d07d9332
NativeCode <tm.nicolasmachuca@gmail.com>
2026-04-13 09:02:49 -0400

    Clinica HOI Auth

--- controller/BloqueoAgendaController.js
+++ controller/BloqueoAgendaController.js
@@ -1,39 +1,39 @@
 import BloqueoAgenda from "../model/BloqueoAgenda.js";
 
 export default class BloqueoAgendaController {
-    constructor(){
+    constructor() {
     }
 
     //InsertarBloqueo
-    static async InsertarBloqueoController(req, res){
+    static async InsertarBloqueoController(req, res) {
         try {
-            const {id_profesional,fechaInicio,horaInicio,fechaFinalizacion,horaFinalizacion,motivo } = req.body;
+            const { id_profesional, fechaInicio, horaInicio, fechaFinalizacion, horaFinalizacion, motivo } = req.body;
             console.log('INSERTAR BLOEQUEO CONTROLLER REQ.BODY :')
             console.log(req.body);
             console.log(" ");
 
             const BloqueoAgendaClass = new BloqueoAgenda();
-            const respuestaModel = await BloqueoAgendaClass.insertarBloqueoAgendaModel(id_profesional,fechaInicio,horaInicio,fechaFinalizacion,horaFinalizacion,motivo);
+            const respuestaModel = await BloqueoAgendaClass.insertarBloqueoAgendaModel(id_profesional, fechaInicio, horaInicio, fechaFinalizacion, horaFinalizacion, motivo);
 
-            if(Array.isArray(respuestaModel) && respuestaModel.length === 0){
-                return res.status(200).send({message : "sindisponibilidad"});
+            if (Array.isArray(respuestaModel) && respuestaModel.length === 0) {
+                return res.status(200).send({ message: "sindisponibilidad" });
             }
-            if(respuestaModel.affectedRows > 0){
-                return res.status(200).send({message : true});
-            }else{
-                return res.status(200).send({message : false});
+            if (respuestaModel.affectedRows > 0) {
+                return res.status(200).send({ message: true });
+            } else {
+                return res.status(200).send({ message: false });
             }
-        }catch(err){
-                return res.status(400).send({
-                    message: 'serverError',
-                })
+        } catch (err) {
+            return res.status(400).send({
+                message: 'serverError',
+            })
         }
     }
 
 
     static async eliminarBloqueoController(req, res) {
         try {
-            const {id_bloqueo} = req.body;
+            const { id_bloqueo } = req.body;
             console.log('ELIMINAR BLOEQUEO CONTROLLER REQ.BODY :')
             console.log(req.body);
             console.log(" ");
@@ -41,20 +41,20 @@ export default class BloqueoAgendaController {
             const BloqueoAgendaClass = new BloqueoAgenda();
             const respuestaModel = await BloqueoAgendaClass.eliminarBloqueoAgenda(id_bloqueo);
 
-            if(respuestaModel.affectedRows > 0){
-                return res.status(200).send({message : true});
-            }else{
-                return res.status(200).send({message : false});
+            if (respuestaModel.affectedRows > 0) {
+                return res.status(200).send({ message: true });
+            } else {
+                return res.status(200).send({ message: false });
             }
-        }catch(err){
-            return res.status(400).send({message : "serverError"});
+        } catch (err) {
+            return res.status(400).send({ message: "serverError" });
         }
     }
 
 
     static async seleccionarBloqueosPorProfesional(req, res) {
         try {
-            const {id_profesional} = req.body;
+            const { id_profesional } = req.body;
             console.log('SELECCIONAR POR PROFESIONAL BLOEQUEO CONTROLLER REQ.BODY :')
             console.log(req.body);
             console.log(" ");
@@ -62,30 +62,30 @@ export default class BloqueoAgendaController {
             const BloqueoAgendaClass = new BloqueoAgenda();
             const respuestaModel = await BloqueoAgendaClass.seleccionarBloqueoPorProfesionalModel(id_profesional);
 
-            if(respuestaModel){
+            if (respuestaModel) {
                 return res.status(200).send(respuestaModel);
             }
-        }catch(err){
-            return res.status(400).send({message : "serverError"});
+        } catch (err) {
+            return res.status(400).send({ message: "serverError" });
         }
     }
 
 
     static async seleccionarBloqueosEntreFechasController(req, res) {
         try {
-            const {fechaInicio,fechaFinalizacion} = req.body;
+            const { fechaInicio, fechaFinalizacion } = req.body;
             console.log('SELECCIONAR POR ENTRE FECHAS BLOEQUEO CONTROLLER REQ.BODY :')
-            console.log(fechaInicio,fechaFinalizacion);
+            console.log(fechaInicio, fechaFinalizacion);
             console.log(" ");
 
             const BloqueoAgendaClass = new BloqueoAgenda();
-            const respuestaModel = await BloqueoAgendaClass.seleccionarBloqueoPorFechasModel(fechaInicio,fechaFinalizacion);
+            const respuestaModel = await BloqueoAgendaClass.seleccionarBloqueoPorFechasModel(fechaInicio, fechaFinalizacion);
 
-            if(respuestaModel){
+            if (respuestaModel) {
                 return res.status(200).send(respuestaModel);
             }
-        }catch(err){
-            return res.status(400).send({message : "serverError"});
+        } catch (err) {
+            return res.status(400).send({ message: "serverError" });
         }
     }
 
@@ -98,11 +98,11 @@ export default class BloqueoAgendaController {
         try {
             const BloqueoAgendaClass = new BloqueoAgenda();
             const respuestaModel = await BloqueoAgendaClass.seleccionarTodosLosBloqueos();
-            if(respuestaModel){
+            if (respuestaModel) {
                 return res.status(200).send(respuestaModel);
             }
-        }catch(err){
-            return res.status(400).send({message : "serverError"});
+        } catch (err) {
+            return res.status(400).send({ message: "serverError" });
         }
     }
 }
